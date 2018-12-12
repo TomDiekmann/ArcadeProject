e9e29000769e3365e36e15f8be47dbbc0a80c783
alessio1309 <32935618+alessio1309@users.noreply.github.com>
2018-12-12 09:54:21 +0100

    Tron zum Menü hinzugefügt

--- ArcadeDesktop/src/Menu/MainState.java
+++ ArcadeDesktop/src/Menu/MainState.java
@@ -25,6 +25,7 @@ public class MainState extends State {
 		games.add(new GameView("Snake", GameStateManager.SNAKEGAMESTATE));
 		games.add(new GameView("Pong", GameStateManager.PONGSTATE));
 		games.add(new GameView("Frogger", GameStateManager.FROGGERSTATE));
+		games.add(new GameView("Tron", GameStateManager.TRONSTATE));
 	}
 
 	@Override
