9aee1b5b1a35f93a703a97a4a5363361e3a955ba
Tom Diekmann <tomdiekmann01@gmail.com>
2018-12-16 17:11:40 +0100

    Mario Todesscreen hinzugefügt

--- ArcadeDesktop/src/SuperMarioBros/MarioWorldState.java
+++ ArcadeDesktop/src/SuperMarioBros/MarioWorldState.java
@@ -28,6 +28,9 @@ public class MarioWorldState extends State {
 	private boolean menuOpen = false;
 	public static int time;
 	private int timeTicks;
+	
+	public static boolean deadScreen;
+	private int deadScreenTicks = 0;
 
 	public MarioWorldState(GameStateManager gsm, String filepath) {
 		super(gsm);
@@ -48,7 +51,7 @@ public class MarioWorldState extends State {
 			timeTicks = 0;
 			time--;
 		}
-		if(!stopTime)
+		if(!stopTime && !deadScreen)
 			timeTicks++;
 	}
 
@@ -56,8 +59,25 @@ public class MarioWorldState extends State {
 	public void render(Graphics2D g) {
 		g.clearRect(0, 0, GamePanel.width / GamePanel.SCALE, GamePanel.height / GamePanel.SCALE);
 
-		world.render(g);
-		player.render(g);
+		if(!deadScreen) {
+			world.render(g);
+			player.render(g);
+		}
+		else {
+			g.setColor(Color.BLACK);
+			g.fillRect(0, 0, GamePanel.width/ GamePanel.SCALE, GamePanel.height/GamePanel.SCALE);
+			g.drawImage(Game.imageLoader.load("images/SuperMarioBros/SmallWalking.png").getSubimage(0, 16, 16, 16), (GamePanel.width / 2 - 75 - 16) / GamePanel.SCALE, (GamePanel.height / 2 - 16) / GamePanel.SCALE, null);
+			g.setColor(Color.white);
+			g.drawString(String.valueOf(player.getLives()),(GamePanel.width / 2 + 75) / GamePanel.SCALE , GamePanel.height / 2 / GamePanel.SCALE + g.getFontMetrics().getHeight() / 2 / GamePanel.SCALE);
+			g.drawLine(GamePanel.width / 2 / GamePanel.SCALE - 5, GamePanel.height / 2 / GamePanel.SCALE - 5, GamePanel.width / 2 / GamePanel.SCALE + 5, GamePanel.height / 2 / GamePanel.SCALE + 5);
+			g.drawLine(GamePanel.width / 2 / GamePanel.SCALE - 5, GamePanel.height / 2 / GamePanel.SCALE + 5, GamePanel.width / 2 / GamePanel.SCALE + 5, GamePanel.height / 2 / GamePanel.SCALE - 5);
+			deadScreenTicks++;
+			if(deadScreenTicks > 150) {
+				Game.gamepanel.gsm.setState(GameStateManager.MARIOWORLD);
+				deadScreen = false;
+				player.reduceLives();
+			}
+		}
 		
 		g.setFont(new Font("Arial Black", 10 ,10));
 		g.setColor(Color.WHITE);
